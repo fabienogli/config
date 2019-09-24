2aafb4fb186583cd9ba8719a4f5da6f8d4b93719
neibaf <fabien.ogli@gmail.com>
2019-09-24 15:47:41 +0200

    update Readme and source_me.sh for better install from scratch

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Custom Configuration for bash files and scripts  
-This file need to be imported in .zhrc or .bashrc
-And this script display an ascii picture
-  
+You need to source the file ```source_me.sh``` in your bashrc or zshrc depending on your terminal
+And this script display an ascii picture with is in the ascii_art folder. To add your own picture, paste it in the ascii_art folder and modify the ```source_me.sh``` script
+
+* Requirements
+You need to put the folder config in your HOME folder, if you don't know it, just type ```printenv HOME```  
+
+You also need to change the git credential, edit ```source_me.sh```  
 NB: this repo is heavely inspired/copied by https://github.com/jessfraz/dotfiles
--- source_me.sh
+++ source_me.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/zsh
-export BACKUP=$HOME/config
-export EDITOR='vim'										# export editor
-export GOPATH=$HOME/go									# set work directory for go
-export GIT="fabienogli"									# export git credential
-export node=$HOME/.npm
-export STARTER=$HOME/starter
-export JOB=$GOPATH/src/gitlab.com/fabienogli/cover-letter
-export PATH=$PATH:$HOME/.shortcuts/bin:$GOPATH/bin:$JOB/bin:$node/bin
-
+export CONFIG=$HOME/config
+# edit this file
+alias config="vim $CONFIG/source_me.sh"
+# export git credential
+export GIT="fabienogli"
+# export editor
+export EDITOR='vim'
+# set work directory for go
+export GOPATH=$HOME/go
 # display picture
 cat $CONFIG/ascii_art/godfather.txt
 
@@ -16,8 +16,12 @@ source $CONFIG/aliases
 source $CONFIG/tmp_aliases
 source $CONFIG/func
 
+#@TODO need to add conditional statement if node/git/starter/coverletter is installed
+export NODE=$HOME/.npm
+export STARTER=$HOME/starter
+export JOB=$GOPATH/src/gitlab.com/fabienogli/cover-letter
+export PATH=$PATH:$HOME/.shortcuts/bin:$GOPATH/bin:$JOB/bin:$NODE/bin
+
 #others
 source $STARTER/.aliases
 
-# edit this file
-alias config="vim $CONFIG/source_me.sh"
