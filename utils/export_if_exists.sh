62589bd2b2f8e5b421bbf655d66f0d74ee78c122
fabienogli <fabien.ogli@etu.univ-lyon1.fr>
2019-11-01 15:31:37 +0100

    update exports

--- utils/export_if_exists.sh
+++ utils/export_if_exists.sh
@@ -1,20 +1,36 @@
 #!/usr/bin/zsh
-# Set GOPATH var because we need it for JOB
-GOPATH=$HOME/go
-declare -A env
-env[NODE]=$HOME/.npm
-env[STARTER]=$HOME/starter
-env[GOPATH]=$GOPATH
-
-for k in "${(@k)env}"; do
-    val=$env[$k]
-    if [ -d "$val" ]; then
-        export $k=$val
-        export PATH=$PATH:$val/bin
+declare -A envs
+envs=(
+	NODE $HOME/.npm 
+	STARTER $HOME/starter	
+	GOPATH $HOME/go
+	)
+	
+for env  in "${(@k)envs}"; do
+	link="$envs[$env]"
+    if [ -d "$link" ]; then
+        export $env=$link
     fi
 done
 
+# export for no env value with bin folder
+declare -a exports
+exports=(
+	$HOME/flutter
+	$GOPATH
+	$STARTER
+	)
+
+for path in "${exports}"; do
+	BIN="$path/bin"
+	if [ -d "$BIN" ]; then
+		PATH=$PATH:$BIN
+	fi
+done
+
+export PATH=$PATH
+
 # source starter if it exists
 if [ ! -z "$STARTER" ]; then
-    source $STARTER/.aliases
+    #source $STARTER/.aliases
 fi
