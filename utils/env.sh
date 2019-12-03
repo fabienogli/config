7eaba4755605e5d31db7264cc25a68c4aa2678fe
fabienogli <fabien.ogli@etu.univ-lyon1.fr>
2019-12-03 11:25:00 +0100

    separate env and path

---
+++ utils/env.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/zsh
+declare -A envs
+envs=(
+	NODE $HOME/.npm 
+	STARTER $HOME/starter	
+	GOPATH $HOME/go
+	)
+	
+for env  in "${(@k)envs}"; do
+	link="$envs[$env]"
+    if [ -d "$link" ]; then
+        export $env=$link
+    fi
+done
--- utils/export_if_exists.sh
+++ utils/export_if_exists.sh
@@ -1,20 +1,6 @@
-#!/usr/bin/zsh
-declare -A envs
-envs=(
-	NODE $HOME/.npm 
-	STARTER $HOME/starter	
-	GOPATH $HOME/go
-	)
-	
-for env  in "${(@k)envs}"; do
-	link="$envs[$env]"
-    if [ -d "$link" ]; then
-        export $env=$link
-    fi
-done
-
-# export PATH 
-export PATH=$PATH:$HOME/flutter/bin:$GOPATH/bin:$HOME/.gem/ruby/2.6.0/bin
+#! /bin/sh
+source $CONFIG/utils/env.sh
+source $CONFIG/utils/path.sh
 
 # source starter if it exists
 if [ ! -z "$STARTER" ]; then
