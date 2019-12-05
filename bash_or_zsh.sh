67c6e4532f2cf09c27d66d826e88071d1ad8db83
fabienogli <fabien.ogli@gmail.com>
2019-12-05 13:31:25 +0100

    ok

--- bash_or_zsh.sh
+++ bash_or_zsh.sh
@@ -1,9 +1,2 @@
-#! /bin/sh
-
-if [ -n "$ZSH_VERSION" ]; then
-   # assume Zsh
-elif [ -n "$BASH_VERSION" ]; then
-   # assume Bash
-else
-   # asume something else
-fi
+#! /bin/bash
+echo "seems that asking is the way"
