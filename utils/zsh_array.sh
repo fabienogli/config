6c2a9683f5ceab86ceb18a8b1746bd74042e4e89
fabienogli <fabien.ogli@gmail.com>
2019-12-09 08:09:03 +0100

    add script to iterate through array with callbackwq!

---
+++ utils/zsh_array.sh
@@ -0,0 +1,17 @@
+#! /usr/bin/zsh
+
+if [[ -z "$2" ]]; then
+   	printf "Apply function for each element of an associative array\n"
+	printf "Usage: $0 array function\n"
+	exit 1
+fi
+
+arr=$1
+
+func=$2
+	
+for element  in "${(@k)arr}"; do
+	associate_element="$arr[$element]"
+	$func($element,$associate_element)
+done
+
